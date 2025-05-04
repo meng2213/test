6cc5935388430256713aca68568d8b52a6acfadf
meng2213 <1349169592@qq.com>
2025-05-04 12:54:41 +0800

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -5,14 +5,14 @@
 const config = {
   // title will be displayed on the top of your site
   title: "一个简单的网页",
-  date: 2025-05-04               
+  date: "2025-05-04",               
   // Use YYYY-MM-DD format
   // adding a description helps with SEO
   description: "开发测试",
   // author of site displayed on the bottom of your site
   author: "M",
   // logo image
-  authorLogo: "/content/assets/logo.jpg",
+  authorLogo: "/public/logo.jpg",
   // url to author website
   navLinks: [{ href: "/about", name: "不要点进来" }],
   // 添加 showSidebar 配置
